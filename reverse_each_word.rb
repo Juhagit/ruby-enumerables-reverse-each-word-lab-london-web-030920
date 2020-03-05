a53d578e80b3beee8031b89b703da42a93f4612e
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-03-05 09:21:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 
-def reverse_each_word(sentence1)
+def reverse_each_word(sentence)
  
- new_array = []
-  new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
+ 
 end
   
-end 
